3ca7a03a572dca1f18beb08c6aa4b2a4df92defc
Sanjeet Jagdev <ssj256x@outlook.com>
2025-11-10 23:17:57 +0530

    Adding methods from APIs

--- backend/sakila/src/main/java/com/backend/sakila/services/FilmService.java
+++ backend/sakila/src/main/java/com/backend/sakila/services/FilmService.java
@@ -1,5 +1,7 @@
 package com.backend.sakila.services;
 
+import com.backend.sakila.api.model.Film;
+import com.backend.sakila.mappers.FilmAndFilmEntityMapper;
 import com.backend.sakila.model.entity.CategoryEntity;
 import com.backend.sakila.model.entity.LanguageEntity;
 import com.backend.sakila.repository.CategoryRepository;
@@ -17,6 +19,7 @@ public class FilmService {
     private final LanguageRepository languageRepository;
     private final CategoryRepository categoryRepository;
     private final FilmRepository filmRepository;
+    private final FilmAndFilmEntityMapper filmAndFilmEntityMapper;
 
     public List<String> getAllLanguages() {
         return languageRepository.findAll().stream()
@@ -36,5 +39,9 @@ public class FilmService {
         return filmRepository.findAllRatings().stream()
                 .map(String::trim)
                 .toList();
+    }
+
+    public Film findFilmById(Integer id) {
+        return filmRepository.findById(id).map(filmAndFilmEntityMapper::filmEntityToFilm).orElseThrow();
     }
 }
